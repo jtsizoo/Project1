e19c936b24c444c554fd88957b0bb9f264f631b9
Justin Sizoo <sizoo.justin@ku.edu>
2022-02-04 14:55:01 -0600

    add edge case to ensure correct # of ships

--- test/main.cpp
+++ test/main.cpp
@@ -2,9 +2,10 @@
 
 int main(int argc, char* argv[]) {
     //run game: ./prog {# of ships}
-    if (argc != 2) {
-        std::cout << "Invalid number of parameters\n";
-    } else {
+    if (argc != 2) std::cout << "Invalid number of parameters\n";
+	else if(atoi(argv[1]) < 1 || atoi(argv[1]) > 5) std::cout << "Invalid number of ships!\n";
+	else
+	{
         int numShips = atoi(argv[1]);
         Executive exec(numShips);
         exec.run();
