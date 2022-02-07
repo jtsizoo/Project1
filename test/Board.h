797902a22a75199f5803f8ea89bd31fc0761b9f2
Maggie Swartz <margaret_swartz@ku.edu>
2022-02-06 19:37:23 -0600

    Added EECS_268 style header and method description skeleton to Board.h

--- test/Board.h
+++ test/Board.h
@@ -1,26 +1,88 @@
+/*-------------------------------------------------------
+ *      File Name: Board.h
+ *      Authors: Alice Kuang, Thresa Kelly, Minwood Lee, Justin Sizoo, Maggie Swartz (Group #14)
+ *      Assignment: EECS_448 Project #1
+ *      Description: This is the header file for the Board class
+ *      Date Last Modified: 02/06/2022
+ *-----------------------------------------------------*/
+
 #ifndef BOARD_H
 #define BOARD_H
 
 #include <iostream>
 
 class Board {
-    private:
+	private:
 	std::string player;
-    int m_size;
-    char** placeGrid;
-    char** shotGrid;
+	int m_size;
+	char** placeGrid;
+	char** shotGrid;
 	int printCol[10] = {1,2,3,4,5,6,7,8,9,10};
 	char printRow[10] = {'A','B','C','D','E','F','G','H','I','J'};
-
-    public:
-    Board(int size, std::string playerNum);
-    ~Board();
-    bool insertShip(int size, int row, int col, char dir);
-    bool shootShot(int row, int col, Board* opBoard);
-    bool isHit(int row, int col);
-    bool checkWin();
-    void printShotGrid();
-    void printPlaceGrid();
+	
+	public:
+	/*----------
+     	* @pre
+     	* @post
+     	* @param
+     	* @return
+     	* -------*/
+	Board(int size, std::string playerNum);
+	/*----------
+        * @pre
+        * @post
+        * @param
+        * @return
+        * -------*/
+	~Board();
+	/*----------
+        * @pre
+        * @post
+        * @param
+        * @return
+        * -------*/
+	bool insertShip(int size, int row, int col, char dir);
+	/*----------
+        * @pre
+        * @post
+        * @param
+        * @return
+        * -------*/
+	bool shootShot(int row, int col, Board* opBoard);
+	/*----------
+        * @pre
+        * @post
+        * @param
+        * @return
+        * -------*/
+	bool isHit(int row, int col);
+	/*----------
+        * @pre
+        * @post
+        * @param
+        * @return
+        * -------*/
+	bool checkWin();
+	/*----------
+        * @pre
+        * @post
+        * @param
+        * @return
+        * -------*/
+	void printShotGrid();
+	/*----------
+        * @pre
+        * @post
+        * @param
+        * @return
+        * -------*/
+	void printPlaceGrid();
+	/*----------
+        * @pre
+        * @post
+        * @param
+        * @return
+        * -------*/
 	void printInitialBoard();
 };
 
