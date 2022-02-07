e9631bed20a477c785ab8c8bf99496f36320f2ac
Maggie Swartz <margaret_swartz@ku.edu>
2022-02-06 19:10:23 -0600

    Added EECS_268 style header and method description skeleton.

--- test/Executive.h
+++ test/Executive.h
@@ -1,3 +1,11 @@
+/*-------------------------------------------------------
+ * 	File Name: Executive.h
+ * 	Authors: Alice Kuang, Thresa Kelly, Minwood Lee, Justin Sizoo, Maggie Swartz (Group #14)
+ * 	Assignment: EECS_448 Project #1
+ * 	Description: This is the header file for the Executive class
+ * 	Date Last Modified: 02/06/2022
+ *-----------------------------------------------------*/
+
 #ifndef EXECUTIVE_H
 #define EXECUTIVE_H
 
@@ -13,12 +21,54 @@ class Executive {
     bool PTurn;
 
     public:
+    /*----------
+     * @pre
+     * @post
+     * @param
+     * @return
+     * -------*/
     Executive(int numShips);
+     /*----------
+     * @pre
+     * @post
+     * @param
+     * @return
+     * -------*/
     ~Executive();
+     /*----------
+     * @pre
+     * @post
+     * @param
+     * @return
+     * -------*/
     void chooseShipLoc(Board* board, int numShips);
+     /*----------
+     * @pre
+     * @post
+     * @param
+     * @return
+     * -------*/
     void run();
+     /*----------
+     * @pre
+     * @post
+     * @param
+     * @return
+     * -------*/
     int charToInt(char c);
+     /*----------
+     * @pre
+     * @post
+     * @param
+     * @return
+     * -------*/
     std::string validateLoc(std::string input);
+     /*----------
+     * @pre
+     * @post
+     * @param
+     * @return
+     * -------*/
     char validateDirection(char input);
 };
 
