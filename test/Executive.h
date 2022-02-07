af25a99f6643734a69b546ab8637c6401653e295
Maggie Swartz <margaret_swartz@ku.edu>
2022-02-07 16:37:40 -0600

    Updated Executive.h to revert back to 1 ship selection

--- test/Executive.h
+++ test/Executive.h
@@ -24,10 +24,10 @@ class Executive {
     		/*----------
      		* @pre numShips >=1 && <= 5
      		* @post constructor creates an instance of the Executive class
-     		* @param int numShips (1-5, chosen # of battleships for first player), int opShips (1-5, chosen # of battleships for second player)
+     		* @param int numShips (1-5, chosen # of battleships for first player)
      		* @return no true return, just creates instance of the class
      		* -------*/
-    		Executive(int numShips, int opShips);
+    		Executive(int numShips);
      		/*----------
      		* @pre an instance of Executive class must exist to be deleted
      		* @post Executive class instance is deleted
