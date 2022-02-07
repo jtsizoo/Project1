b5e48586992981f5b393e4c6ed57c40cf18f10b5
Alice Kuang <yuchun.kuang@gmail.com>
2022-02-07 13:07:24 -0600

    Updated program to accept an additional argument for second player's ship selection

--- test/main.cpp
+++ test/main.cpp
@@ -7,17 +7,27 @@
  *-----------------------------------------------------*/
 
 #include "Executive.h"
+//void welcomeMsg() {
+//    std::cout << "\n\nWelcome to Battleship!\n\n";
+//    std::cout << "Select your ships and take turns guessing your opponents' ship locations.\n";
+//    std::cout << "If you hit one of your opponent's ships, you will be rewarded with another turn.\n";
+//    std::cout << "First player to sink all their opponent's ships wins.\n\n";
+//}
 
 int main(int argc, char* argv[]) { //To run the program, user must enter ./test and the number of ships they'd like to play with.
-	if (argc != 2) { //If the user does not enter 2 arguments into the command line, the below error message will display.
-		std::cout << "Invalid number of parameters! To run the program, enter ./test followed by the number of desired ships (1, 2, 3, 4, or 5).\n";
+	//welcomeMsg();	
+	if (argc != 3) { //If the user does not enter 3 arguments into the command line, the below error message will display.
+		std::cout << "Invalid number of parameters! To run the program, enter ./test followed by the number of desired ships (1, 2, 3, 4, or 5). for two players\n";
 	} else { //When the user has etered ./test and a second argument into the command line, they enter the below if/else loop.
-		if (atoi(argv[1]) < 1 || atoi(argv[1]) > 5) { //If the 2nd argument converted to an integer is not between 1 and 5 inclusive, display error message.
+		if (atoi(argv[1]) < 1 || atoi(argv[1]) > 5||atoi(argv[2]) < 1 || atoi(argv[2]) > 5) { //If the 2nd and 3rd argument converted to an integer is not between 1 and 5 inclusive, display error message.
                         std::cout << "Invalid number of ships! Must be 1, 2, 3, 4, or 5.\n";
 		} else { //If the 2nd argument converted to an integer is between 1 and 5 inclusive...
 			int numShips = atoi(argv[1]); //Sets the variable integer numShips equal to the 2nd argument converted to an integer.
-			std::cout << "\nNumber of ships selected: " << numShips << "\n"; //Displays number of ships the user has selected.
-			Executive exec(numShips); //Creates an instance of the Executive class called exec, with numShips passed in as the argument.
+			int opShips = atoi(argv[2]);
+			std::cout << "\nNumber of ships selected for Player 1: " << numShips << "\n"; //Displays number of ships the first player has selected.
+			std::cout << "\nNumber of ships selected for Player 2: " << opShips << "\n"; //Displays number of ships the second player has selected.
+
+			Executive exec(numShips, opShips); //Creates an instance of the Executive class called exec, with numShips passed in as the argument.
 			exec.run(); //Triggers run function within created instance of the Executive class.
 		}
 	}
